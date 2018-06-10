cbb5d0d3f57df119dc9fbf20446fc3d1266ef85e
hhiroki <h-hiroki@users.noreply.github.com>
2018-06-10 16:19:18 +0900

    検索内容をURLパラメタに付与する

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import _ from 'lodash';
 
 import SearchForm from './SearchForm';
@@ -34,6 +35,7 @@ class SearchPage extends Component {
   }
 
   handlePlaceSubmit(place) {
+    this.props.history.push(`/?query=${place}`);
     geocode(place) // call Google Map API.
     .then(({ status, address, location }) => {
       switch (status) {
@@ -52,7 +54,6 @@ class SearchPage extends Component {
       return [];
     })
     .then((hotels) => {
-      console.log(hotels);
       this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
     })
     .catch(() => {
@@ -91,5 +92,9 @@ class SearchPage extends Component {
     );
   }
 }
+
+SearchPage.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
 
 export default SearchPage;
